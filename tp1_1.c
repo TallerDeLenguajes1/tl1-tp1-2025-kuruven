ce9c8b299fa0da6aef9bbaecd02b94c9a4227dd0
Agustin <agustincastillo504@gmail.com>
2026-04-16 15:19:31 -0300

    agregando el codigo tp1_1

--- tp1_1.c
+++ tp1_1.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
 
+void mostrar(int *numero, int **direccion, int *puntero){
+    printf("el numero es %d \n",*numero);
+    printf("La direccion en memoria de la variable es: %p \n", numero);
+    printf("La direccion en memoria almacenada por el puntero es: %p \n", puntero);
+    printf("La direccion de memoria del puntero es: %p \n", direccion);
+    printf("el tamaño utilizado por la variable es: %d \n", sizeof(*numero));
+}
+
 int main()
 {
     printf("hola mundo");
-    
+    int *puntero ,numero=0;
+    numero = 27;
+    puntero= &numero;
+    mostrar(&numero, &puntero, puntero);
     return 0;
 }
